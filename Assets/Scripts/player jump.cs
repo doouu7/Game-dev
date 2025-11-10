2d321c97eac935f9685868ce156d2c195ad135dd
doouu7 <mamdouhfkh@hotmail.com>
2025-11-10 16:54:59 +0000

    Added my Assets l2,3,4

---
+++ Assets/Scripts/player jump.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    public float jumpForce = 7f;
+    private Rigidbody2D rb;
+    private bool isGrounded = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        // reset vertical velocity before jumping
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        isGrounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
